775e67cde6e4f227bf9dfab941b66e924a360f48
Dhwanit10 <dhwanitakoliya10@gmail.com>
2025-11-16 21:49:31 +0530

    added config for deployment

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ builder.Services.AddSignalR();
 
 builder.Services.AddSingleton<UniverseManager>();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
